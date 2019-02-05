1161885c8a182ce60bfdbe9d051fde5a9e5a26cc
Pok Tik Benjamin Leung <benminleung@gmail.com>
2019-02-05 22:19:57 +0200

    router

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Router>
+        <Router basename={`${process.env.PUBLIC_URL}/`}>
           <div className="bracket">
             <SecHeader />
             <Switch>
